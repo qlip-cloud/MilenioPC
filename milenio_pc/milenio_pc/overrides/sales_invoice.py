17671a4ee6e2e782f8d63306dde6dab9a20faa7a
HendersonMentum <henderson.villegas@mentum.group>
2023-08-17 20:15:29 +0000

    add changes

--- milenio_pc/milenio_pc/overrides/sales_invoice.py
+++ milenio_pc/milenio_pc/overrides/sales_invoice.py
@@ -9,3 +9,13 @@ class CustomSalesInvoice(SalesInvoice):
 		name = self.naming_series + self.sequence
 		parts = name.split('.')
 		self.name = parse_naming_series(parts)  
+
+	def calculate_taxes_and_totals(self):
+
+		from .custom_calculate_taxes_and_totals import custom_calculate_taxes_and_totals
+
+		custom_calculate_taxes_and_totals(self)
+
+		if self.doctype in ["Quotation", "Sales Order", "Delivery Note", "Sales Invoice"]:
+			self.calculate_commission()
+			self.calculate_contribution()
