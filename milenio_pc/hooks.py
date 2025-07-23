d3a8b83a15d8c93e1f61cee9de4776451fae3c2e
afuentes-alphas <afuentes.mentum@gmail.com>
2025-07-23 19:49:57 -0400

    [MOD] Se omite la creación de indices. Se traslada al erp base.

--- milenio_pc/hooks.py
+++ milenio_pc/hooks.py
@@ -62,7 +62,8 @@ doctype_js = {
 # before_install = "milenio_pc.install.before_install"
 # after_install = "milenio_pc.install.after_install"
 
-after_migrate = "milenio_pc.milenio_pc.use_case.account.add_index"
+# Se comenta para trasladarlo al ERP BASE
+# after_migrate = "milenio_pc.milenio_pc.use_case.account.add_index"
 
 # Desk Notifications
 # ------------------
